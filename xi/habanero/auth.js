6c51188d4d4d60ad4ac292a84ded0021a748419f
Matthew Sullivan <msullivan@msullivan-mbp.3amlabs.net>
2016-03-22 15:37:51 -0400

    fix tab spacing

--- xi/habanero/auth.js
+++ xi/habanero/auth.js
@@ -10,55 +10,33 @@ redisClient.on("error", function (err) {
 
 var idm = require('../services/idm');
 var blueprint = require('../services/blueprint');
-
 var habaneroSettings = require('./settings');
 
 var RED = require("../../../../red/runtime");
 
 
-
-var getCurrentAuth = function(){
-	redisClient.get(XI_CREDS_KEY, function(err, reply) {
-        if (!err) {
-            if (reply === null) {
-                return false;
-            }
-            xiCreds = JSON.parse(reply);
-            hasXiAuth = true;
-            return true;
-        }
-        log.info("Error initializing xi creds: " + err);
-        return false;
-    });
-};
-
-var setCurrentAuth = function(creds){
-	xiCreds = creds;
-	hasXiAuth = true;
-};
-
 var createHabaneroIdmUser = function(xiAccountId, xiAppId, xiAccessToken){
     return when.promise(function(resolve, reject) {
-	    var loop = 0;
-	    var pw = uuid.v4();
-	    var email = null;
+        var loop = 0;
+        var pw = uuid.v4();
+        var email = null;
 
-	    function createUser(){
-	    	email = "habanero_"+loop+"@"+xiAccountId+".com";
-	    	return idm.auth.createUser(email, pw, xiAccountId, xiAppId, xiAccessToken).then(function(resp){
-	    		if(resp['emailAddress'] === email){
-	    			resolve({email:email, password:pw, userId:resp["userId"]});
-	    		}else{
-	    			loop++;
-	    			if(loop>75){
-	    				return reject("too many habanero users");
-	    			}
-	    			return createUser();
-	    		}
-	    	});
-	    }
-	    createUser().then(resolve);
-	});
+        function createUser(){
+            email = "habanero_"+loop+"@"+xiAccountId+".com";
+            return idm.auth.createUser(email, pw, xiAccountId, xiAppId, xiAccessToken).then(function(resp){
+                if(resp['emailAddress'] === email){
+                    resolve({email:email, password:pw, userId:resp["userId"]});
+                }else{
+                    loop++;
+                    if(loop>75){
+                        return reject("too many habanero users");
+                    }
+                    return createUser();
+                }
+            });
+        }
+        createUser().then(resolve);
+    });
 };
 
 var setupDefaultFlows = function(habaneroIdmUser){
@@ -85,34 +63,33 @@ var setupDefaultFlows = function(habaneroIdmUser){
 
 var setupHabaneroAuth = function(jwt, xiAccountId, xiAppId, xiAccessToken){
     var habaneroIdmUserCreds;
-	return when.promise(function(resolve, reject) {
-		createHabaneroIdmUser(xiAccountId, xiAppId, xiAccessToken).then(function(idmUser){
-			habaneroIdmUserCreds = {
+    return when.promise(function(resolve, reject) {
+        createHabaneroIdmUser(xiAccountId, xiAppId, xiAccessToken).then(function(idmUser){
+            habaneroIdmUserCreds = {
                 creds_name: "HabaneroUser",
-				account_id: xiAccountId,
-				user_id: idmUser.userId,
-				username: idmUser.email,
-				password: idmUser.password
-			};
+                account_id: xiAccountId,
+                user_id: idmUser.userId,
+                username: idmUser.email,
+                password: idmUser.password
+            };
             return blueprint.accountUsers.post(xiAccountId, jwt);
         }).then(function(createAccountUserResp){
             habaneroIdmUserCreds.account_user_id = createAccountUserResp.accountUser.id;
             return setupDefaultFlows(habaneroIdmUserCreds);
         }).then(function(){
-            console.log("Should resolve here");
-			return resolve(habaneroIdmUserCreds);
-		}).catch(function(err){
-			console.log("setupXiAuth err: "+err);
+            return resolve(habaneroIdmUserCreds);
+        }).catch(function(err){
+            console.log("setupXiAuth err: "+err);
             reject(err);
-		});
-	});
+        });
+    });
 };
 
 var loginUser = function(username, password, accountId){
-  return idm.auth.loginUser(username, password, accountId);
+    return idm.auth.loginUser(username, password, accountId);
 };
 
 module.exports = {
-	loginUser:loginUser,
+    loginUser:loginUser,
    	setupHabaneroAuth: setupHabaneroAuth
 };
