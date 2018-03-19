127b8c1ccfc199d29cf06bc9b454dfc4b8ae0772
Gavin Berkowitz <gberkowitzLMI@users.noreply.github.com>
2018-03-19 14:06:09 -0400

    rm lmi

--- nodes/salesforce.js
+++ nodes/salesforce.js
@@ -1,5 +1,5 @@
  /**
- * Copyright 2016 LogMeIn Inc.
+ * Copyright 2016 Xively
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
